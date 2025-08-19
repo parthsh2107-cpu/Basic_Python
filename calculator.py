0f1aa5836ef33e273fd3974e03d19921a410b5a0
Parth Sharma <parthsh2107@gmail.com>
2025-08-19 16:13:47 +0530

    Add files via upload
    PROJECT - "SIMPLE CALCULATOR"

---
+++ calculator.py
@@ -0,0 +1,18 @@
+
+# calculator 
+m =  int(input("enter first number:"))
+n =  int(input("enter second number:"))
+print(f"addition: {m} + {n} = {m+n}")
+print(f"subtraction: {m} - {n} = {m-n}")
+print(f"multiplication: {m} * {n} = {m*n}")
+print(f"division: {m} / {n} = {m/n}")
+print(f"floor division: {m} // {n} = {m//n}")
+print(f"exponent: {m} ** {n} = {m**n}")
+
+
+
+
+
+
+
+
