e1c19052a33131e5ed06ac244bca2e0c37cb0872
Parth Sharma <parthsh2107@gmail.com>
2025-08-19 19:29:37 +0530

    Add files via upload
    identity operators

---
+++ identityoperators.py
@@ -0,0 +1,19 @@
+#comparison of identity operator using is
+X = ["apple","banana"]
+Y = ["apple","banana"]
+print(X is Y)
+
+# using is not
+X = ["apple","banana"]
+Y = ["apple","banana"]
+print(X is not Y)
+
+#using == operator
+X = ["apple","banana"]
+Y = ["apple","kiwi"]
+print(X == Y)
+    
+
+    
+
+    
