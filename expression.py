293df4e0ffb789330c758c8a701c8ceffe485922
Parth Sharma <parthsh2107@gmail.com>
2025-08-20 20:28:46 +0530

    Add files via upload
    Expression making

---
+++ expression.py
@@ -0,0 +1,11 @@
+a = int(input("First number:"))
+b = int(input("Second number:"))
+c = int(input("Third number:"))
+d = int(input("Fourth number:"))
+e = int(input("Fifth number:"))
+result_1 = a * c + b
+print(result_1)
+result_2 = b * a / d + c
+print(result_2)
+result_3 = a + b + c + d + e 
+print(result_3)
