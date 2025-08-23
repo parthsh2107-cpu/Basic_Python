7e430cbf8324e44621cbaebfa703edad508fb15e
Parth Sharma <parthsh2107@gmail.com>
2025-08-23 14:27:24 +0530

    Add files via upload
    Bitwise operators code practice

---
+++ bitwise.py
@@ -0,0 +1,44 @@
+#Bitwise Operators
+#Bitwise AND Operator
+
+a = 12     # 1100 in binary
+b = 10     # 1010 in binary
+result = a & b # 1 only where both bits are 1
+print(result)
+ # output = 8
+
+#Bitwise OR operator
+
+x = 7
+y = 5
+result = x | y
+print(result)
+
+#Bitwise XOR operator
+
+p = 9
+q = 13
+result = p ^ q
+print(result)
+
+#Bitwise  NOT operator
+
+n = 6
+result = ~n
+print(result)
+    
+#Bitwise Left Shift Operator
+
+# for example value = 8 , perform left shift by 2 position
+
+value = 8
+result = value << 2
+print(result)
+
+#Bitwise Right Shift Operator
+
+#For example value = 14 , perform right shift by 3 position
+
+value = 14
+result = value << 3
+print(result)
