4c61c818ee33e3d9e8694a3f1bea2566d576f6d7
Parth Sharma <parthsh2107@gmail.com>
2025-08-23 15:18:52 +0530

    Add files via upload
    code practice on membership operators

---
+++ membership.py
@@ -0,0 +1,22 @@
+# Membership Operators
+
+# Problem1 - Given a list colors = ["red", "blue", "green"], write a Python program to check if "blue" is in the list and print the result.
+
+#colors = ["red","blue","green"]
+result = "blue" in colors
+print(result)
+
+    
+# Problem2 - Given a list fruits = ["apple", "banana", "orange"], write a Python program to check if "mango" is not in the list and print the result.
+
+fruits = ["apple","banana","orange"]
+result = "mango" not in fruits
+print(result)
+
+# Problem3 -Given a dictionary data = {"name": "Alice", "age": 25}, write a Python program to check if "city" is not in the dictionary keys and print the result.
+
+data = {"name":"Alice", "age":25}
+result = "city" not in data
+print(result)
+
+
